3ce4d4cca5788a9a648e8ab92913ac9ac90524e7
= <doronindima997@gmail.com>
2026-09-23 18:47:57 +0500

    adjusted settings and converted static web-pages to django templates with dynamicly changing data

--- myproject/planets/views.py
+++ myproject/planets/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .data import PLANETS, PLANETS_ORDER
 
-# Create your views here.
+def index(request):
+
+    first_slug = PLANETS_ORDER[0]
+    return planet_detail(request, first_slug)
+
+def planet_detail(request, slug):
+
+    if slug not in PLANETS:
+        return render(request, 'planets/404.html', status=404)
+    
+    planet = PLANETS[slug]
+    menu = [(s, PLANETS[s]['name']) for s in PLANETS_ORDER]
+
+    context = {
+        'planet': planet,
+        'menu': menu,
+        'current_slug': slug,
+    }
+    
+    return render(request, 'planets/planet.html', context)
